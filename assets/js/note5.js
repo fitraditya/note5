f539cc9987bdbc0073329b62d5082c5c5f2b45bc
Fitra Aditya <fitra@kreatifmedia.web.id>
2013-08-29 11:21:42 +0700

    Redesign UI using bootstrap 3.0

--- assets/js/note5.js
+++ assets/js/note5.js
@@ -1,37 +1,47 @@
 $(function() {
 
-  var loadFile = function() {
-    var fileTemp = $('#text-open')[0].files[0];
-    var fileReader = new FileReader();
-    fileReader.onload = function(evt) {
-      var fileBuffer = evt.target.result;
-      $('#text-content').val(fileBuffer);
-    };
-    fileReader.readAsText(fileTemp, 'UTF-8');
-  }
+  var win = $(window);
+  var w_win = win.width();
+  var h_win = win.height();
 
-  var saveFile = function() {
-    var textBuffer = $('#text-content').val();
-    var textFile = new Blob([textBuffer], {type:'text/plain'});
-    var fileName = $('#text-filename').val();
+  // Set #text-area height and width
+  var t = $('#text-area');
+  var w_t = w_win - 5;
+  var h_t = h_win - 70;
+  t.css('width', w_t);
+  t.css('height', h_t);
 
-    var fileDownload = document.createElement("a");
-    fileDownload.download = fileName;
-    fileDownload.innerHTML = "Download File";
-    fileDownload.href = window.webkitURL.createObjectURL(textFile);
-    fileDownload.click();
-  }
+  win.resize(function() {
+    w_win = win.width();
+    h_win = win.height();
+    w_t = w_win - 5;
+    h_t = h_win - 70;
+    t.css('width', w_t);
+    t.css('height', h_t);
+  });
 
-  $('#open-file').click(function() {
-    $('#text-open').click();
-    $('#text-open').change(function() {
-      $('#text-file').val($(this).val());
-      loadFile();
+  // Customize input file
+  $('#openFile').click(function() {
+    $('#inputFile').click();
+    $('#inputFile').change(function() {
+      $('#inputFileName').val($(this).val());
     });
   });
 
-  $('#save-file').click(function() {
-    saveFile();
+  // Load file action
+  $('#loadFile').click(function() {
+    loadFile();
   });
 
 });
+
+// Load file function
+var loadFile = function() {
+  var fileTemp = $('#inputFile')[0].files[0];
+  var fileReader = new FileReader();
+  fileReader.onload = function(evt) {
+    var fileBuffer = evt.target.result;
+    $('#text-area').val(fileBuffer);
+  };
+  fileReader.readAsText(fileTemp, 'UTF-8');
+}
